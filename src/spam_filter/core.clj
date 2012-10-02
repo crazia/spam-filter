7376ecc0dc5a717cfa9eed041c6de661804303d6
crazia <crazia@craziaui-MacBook-Air.local>
2012-10-02 23:51:10 +0900

    train function added

--- src/spam_filter/core.clj
+++ src/spam_filter/core.clj
@@ -4,3 +4,89 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+;; 괜찮나 (ham) 안 괜찮나 (spam) 의 기준점을 나누기 
+(def max-ham-score 0.4)
+(def min-spam-score 0.6)
+
+
+;; Global 저장 객체들 선언 
+
+(def database (ref {}))
+(def total-spams (ref 0))
+(def total-hams (ref 0))
+
+
+(defstruct word-feature :word :spam-count :ham-count)
+
+;; 주어진 점수에 따라서 스팸인지 아닌지를 최종 결정하는 함수 
+(defn classification [score]
+  (cond
+    (<= score max-ham-score) 'ham
+    (>= score min-spam-score) 'spam
+    true 'unsure))
+
+
+;; database 에 주어진 작업을 하는 내용 
+;; (defn intern-feature [word]
+;;   (dosync
+;;    (alter database merge {word (struct word-feature word 0 0)})))
+
+;; (defn intern-feature [word]
+;;   (dosync
+;;    (alter database merge {word (struct word-feature word 0 0)}))
+;;   (database word))
+
+(defn intern-feature [word]
+  (let [db (database word)]
+    (if (nil? db)
+      (do       
+        (dosync
+         (alter database merge {word (struct word-feature word 0 0)}))
+        (database word))
+      db)))
+
+;; 크기대로 (영문은 3글자 이상, 한글은 2글자 이상) 글자를 나누는 작업
+;; 중복된 글자들은 한개로 처리 한다. 
+(defn extract-words [text]
+  (distinct (re-seq #"[a-zA-Z]{3,}|[가-힣]{2,}" text)))
+
+
+
+;; 주어진 문장을 전부 쪼개서 데이타베이스에 저장하기 
+(defn extract-features [text]
+  (map intern-feature (extract-words text)))
+
+
+(defn increment-feature-count [feature type]
+  (assoc feature type (inc (feature type))))
+
+;;
+;; (defn increment-count [word type]
+;;   (let [db ((intern-feature word) word) ]
+;;     (dosync
+;;      (alter database merge {word (increment-feature-count db type)}))))
+
+(defn increment-count [word type]
+  (let [db (intern-feature word)]
+    (dosync
+     (alter database merge {word (increment-feature-count db type)}))))
+
+
+
+(defn increment-total-count [type]
+  (case type
+    :ham-count (dosync (alter total-hams inc))
+    :spam-count (dosync (alter total-spams inc))))
+
+(defn clear-database! []
+  (dosync (ref-set database {}) ;; not set but map 
+          (ref-set total-spams 0)
+          (ref-set total-hams 0)))
+
+
+(defn train [text type]
+  (doseq [ feature (extract-features text) ]
+    (increment-count (:word feature) type))
+  (increment-total-count type))
